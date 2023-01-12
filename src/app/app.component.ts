8d7274eeeb8e5669e43e0b3350fa93224d3478b4
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-01-12 19:34:21 +0000

    tabela campeonato done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableService } from './services/table.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-fetchapi';
+export class AppComponent implements OnInit {
+  table!: any;
+
+  ngOnInit(): void{
+    this.table = this.tableService.getTable();
+  }
+
+  constructor(private tableService:TableService){}
 }
---
+++ src/app/services/table.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TableService {
+  key: string = 'test_2bec17255065e582e5bf96cf4a5b9a';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getTable(): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.key}`,
+    });
+
+    const requestOptions = { headers: headers };
+
+    return this.httpClient.get<any>(
+      'https://api.api-futebol.com.br/v1/campeonatos/14/tabela',
+      requestOptions
+    );
+  }
+}
